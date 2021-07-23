77cb751be8748faf4f46433047759143e1ece7b3
Daryna <56389524+NikitenkoDaryna@users.noreply.github.com>
2021-07-23 15:09:03 +0300

    change weatherForecast string

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -1,5 +1,6 @@
 const http = require('http')
 
+//TODO: add new data to forecast
 const forecast = (latitude, longitude, callback) => {
     const url = `http://api.weatherstack.com/current?access_key=8585f01fa9b9614ecd7423f027cd5021&query=${latitude},${longitude}&units=f`
     const request = http.request(url, (response) => {
@@ -11,8 +12,8 @@ const forecast = (latitude, longitude, callback) => {
             const parsedData = JSON.parse(buffer)
             if (parsedData.error)callback(parsedData.error.info, undefined)
             else {
-                const { weather_descriptions, temperature, precip } = parsedData.current
-                const weatherForecast = `${weather_descriptions}.It is currently ${temperature} degrees out. There is ${precip}% chance of the rain`
+                const { weather_descriptions, temperature, precip, feelslike, uv_index} = parsedData.current
+                const weatherForecast = `${weather_descriptions}.It is currently ${temperature} degrees out.Feels like: ${feelslike}.There is ${precip}% chance of the rain. UV index is ${uv_index}`
                 callback(undefined, weatherForecast)
             }
         })
